ef2a034371175e3513273c7366d3e111c5082a86
LPTimey <tim.ruland@icloud.com>
2023-07-27 08:15:39 +0200

    fixed err

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,17 +1,26 @@
-local cmp = require("cmp")
+-- local cmp = require("cmp")
 
 local M = {
+  --{
+  --  "hrsh7th/nvim-cmp",
+  --  opts = function()
+  --    local M = require("plugins.configs.cmp")
+  --    M.completion.completeopt = "menu,menuone,noselect"
+  --    M.mapping["<CR>"] = cmp.mapping.confirm({
+  --      behavior = cmp.ConfirmBehavior.Insert,
+  --      select = false,
+  --    })
+  --    table.insert(M.sources, { name = "crates" })
+  --    return M
+  --  end,
+  --},
   {
     "hrsh7th/nvim-cmp",
-    opts = function()
-      local M = require("plugins.configs.cmp")
-      M.completion.completeopt = "menu,menuone,noselect"
-      M.mapping["<CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = false,
-      })
-      table.insert(M.sources, { name = "crates" })
-      return M
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
     end,
   },
 }
