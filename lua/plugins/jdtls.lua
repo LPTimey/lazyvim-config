baea923a4a305989ffaad76a2a1f5065fcc728e7
Tim Ruland <tim.ruland@icloud.com>
2023-10-10 17:21:50 +0200

    mistake

---
+++ lua/plugins/jdtls.lua
@@ -0,0 +1,18 @@
+local M = {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        jdtls = {},
+      },
+      setup = {
+        jdtls = function()
+          return true -- avoid duplicate servers
+        end,
+      },
+    },
+  },
+}
+
+return M
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,9 @@
+local M = {
+  "williamboman/mason.nvim",
+  opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    vim.list_extend(opts.ensure_installed, { "java-test", "java-debug-adapter" })
+  end,
+}
+
+return M
